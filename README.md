60149cfc861c6a49f0f06db0771380d1b14e8632
sxvxn <unormiesable@gmail.com>
2024-05-06 12:50:25 +0700

    Upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# RSA-Encryption
+# Enkripsi-Dekripsi RSA (Rivest Shamir Adleman)
+
+## Tentang RSA
+Algoritma RSA adalah sebuah algoritma kriptografi kunci publik yang ditemukan oleh Ron Rivest, Adi Shamir, dan Leonard Adleman pada tahun 1978. Algoritma ini sangat penting dalam dunia kriptografi karena menyediakan kerangka kerja untuk komunikasi aman melalui jaringan yang tidak aman, seperti internet.
+
+## Tentang Aplikasi
+Aplikasi ini dibuat dengan tujuan untuk membantu proses Enkripsi-Dekripsi dengan algoritma RSA.
+
+## Tujuan
+Tujuan utama dari pembuatan aplikasi ini adalah untuk menyelesaikan tugas matakuliah Keamanan Informasi yang sedang saya jalani di Semester 4.
+
+## Tentang Saya
+Saya Atthariq Insanulhaq Supiana, seorang mahasiswa teknik informatika Universitas Siliwangi.
---
+++ helper/help.py
@@ -0,0 +1,73 @@
+import random
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError("p and q cannot be equal.")
+    
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+    
+    d = mod_inverse(e, phi)
+    
+    return ((e, n), (d, n))
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def mod_inverse(a, m):
+    m0, x0, x1 = m, 0, 1
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
+
+def encrypt(public_key, plaintext):
+    e, n = public_key
+    cipher = [pow(ord(char), e, n) for char in plaintext]
+    return cipher
+
+def decrypt(private_key, cipher):
+    d, n = private_key
+    plaintext = [chr(pow(char, d, n)) for char in cipher]
+    return ''.join(plaintext)
+
+if __name__ == '__main__':
+    p = 61
+    q = 53
+    public_key, private_key = generate_keypair(p, q)
+    print("Public key:", public_key)
+    print("Private key:", private_key)
+    
+    message = "Hello, RSA!"
+    print("Original message:", message)
+    
+    encrypted_message = encrypt(public_key, message)
+    print("Encrypted message:", encrypted_message)
+    
+    decrypted_message = decrypt(private_key, encrypted_message)
+    print("Decrypted message:", decrypted_message)
