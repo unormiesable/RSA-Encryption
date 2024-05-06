76019e531dfbf8e4d44701e0663794d1b5f535f8
sxvxn <unormiesable@gmail.com>
2024-05-06 14:53:16 +0700

    small changes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pass
-
     return render_template('index.html')
+
+@app.route('/github')
+def github():
+    return redirect("https://github.com/unormiesable/RSA-Encryption")
 
 if __name__ == '__main__':
     app.run(debug=True)
